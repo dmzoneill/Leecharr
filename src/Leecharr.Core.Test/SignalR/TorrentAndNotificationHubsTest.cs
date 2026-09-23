b8d0484b89e5ba144aa9c15cae26a499d7b9602a
David O Neill <dmz.oneill@gmail.com>
2026-09-24 00:05:03 +0100

    fix(test): add missing using Microsoft.AspNetCore.Http.Connections.Features in TorrentAndNotificationHubsTest

--- src/Leecharr.Core.Test/SignalR/TorrentAndNotificationHubsTest.cs
+++ src/Leecharr.Core.Test/SignalR/TorrentAndNotificationHubsTest.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using FluentAssertions;
 using Leecharr.Api.V1.SignalR;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Connections.Features;
 using Microsoft.AspNetCore.Http.Features;
 using Microsoft.AspNetCore.SignalR;
 using NSubstitute;
